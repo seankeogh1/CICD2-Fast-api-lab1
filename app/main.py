59f8cddcb124b0cb44823c471a8d98fafaddabb0
seankeogh1 <150271508+seankeogh1@users.noreply.github.com>
2026-09-21 16:03:22 +0000

    Added Post and Get for Users to main

--- app/main.py
+++ app/main.py
@@ -1,10 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
+
+from app.schema import UserCreate
 
 app = FastAPI(title ="Lab 1 - FastAPI User API")
 
+users: list[UserCreate] = []
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
 @app.get("/hello")
 def hello():
     return{"message": "Some message"}
+
+@app.post("/api/users", status_code=status.HTTP_201_CREATED)
+def add_user(new_user: UserCreate):
+    for existing_user in users:
+        if existing_user.user_id == new_user.user_id:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="A user with this id already exists")
+    users.append(new_user)
+    return new_user
+
+@app.get("/api/users")
+def get_users():
+    return users
+
+#Skipping to the PR
