7fe5991d2435748ce049a17725d900ed6ca5148f
seankeogh1 <150271508+seankeogh1@users.noreply.github.com>
2026-09-25 12:10:19 +0000

    Add user read endpoints Part 8

--- app/main.py
+++ app/main.py
@@ -1,28 +1,47 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status 
+ 
+from app.schemas import UserCreate 
+ 
+app = FastAPI(title="Lab 1 - FastAPI User API") 
+ 
+users: list[UserCreate] = [] 
+ 
+ 
+@app.get("/health") 
+def health(): 
+    return {"status": "ok"} 
+ 
+ 
+@app.get("/hello") 
+def hello(): 
+    return {"message": "Hello from FastAPI"} 
 
-from app.schema import UserCreate
-
-app = FastAPI(title ="Lab 1 - FastAPI User API")
-
-users: list[UserCreate] = []
-@app.get("/health")
-def health():
-    return {"status": "ok"}
-
-@app.get("/hello")
-def hello():
-    return{"message": "Some message"}
-
-@app.post("/api/users", status_code=status.HTTP_201_CREATED)
-def add_user(new_user: UserCreate):
-    for existing_user in users:
-        if existing_user.user_id == new_user.user_id:
-            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="A user with this id already exists")
-    users.append(new_user)
-    return new_user
-
-@app.get("/api/users")
-def get_users():
-    return users
-
-#Skipping to the PR
+@app.get("/api/users") 
+def get_users(): 
+    return users 
+ 
+ 
+@app.get("/api/users/{user_id}") 
+def get_user(user_id: int): 
+    for existing_user in users: 
+        if existing_user.user_id == user_id: 
+            return existing_user 
+ 
+    raise HTTPException( 
+        status_code=status.HTTP_404_NOT_FOUND, 
+        detail="User not found", 
+    ) 
+ 
+ 
+@app.post("/api/users", status_code=status.HTTP_201_CREATED) 
+def add_user(new_user: UserCreate): 
+    for existing_user in users: 
+        if existing_user.user_id == new_user.user_id: 
+            raise HTTPException( 
+                status_code=status.HTTP_409_CONFLICT, 
+                detail="A user with this user_id already exists", 
+            ) 
+        
+ 
+    users.append(new_user) 
+    return new_user 
