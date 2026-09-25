a2f651a32a1fd1d3e453efbe45aaeec2a5d0ef38
seankeogh1 <150271508+seankeogh1@users.noreply.github.com>
2026-09-25 12:24:10 +0000

    Lab1-add-userDelete-Endpoints

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, status 
+from fastapi import FastAPI, HTTPException, Response, status 
  
 from app.schemas import UserCreate 
  
@@ -45,3 +45,15 @@ def add_user(new_user: UserCreate):
  
     users.append(new_user) 
     return new_user 
+
+@app.delete("/api/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT) 
+def delete_user(user_id: int): 
+ for index, existing_user in enumerate(users): 
+  if existing_user.user_id == user_id: 
+     users.pop(index) 
+  return Response(status_code=status.HTTP_204_NO_CONTENT) 
+
+raise HTTPException( 
+status_code=status.HTTP_404_NOT_FOUND, 
+detail="User not found", 
+) 
